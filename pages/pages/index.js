8d9082fa3cca3b3040e9c7d02935760e02a19f43
Om-Vasani <omvasani63@gmail.com>
2025-11-25 10:09:57 +0530

    Delete pages/pages directory

--- pages/pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h1>Client Find Engine</h1>
-      <p>Welcome to the AI Client Finder Engine.</p>
-      <p>Use POST /api/generate to generate AI output.</p>
-    </div>
-  );
-}
